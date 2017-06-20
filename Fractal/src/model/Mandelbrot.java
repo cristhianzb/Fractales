758bc0fcacc364245715611aeec3dcdf78bab571
cristhianzb <cristhianzentenob@gmail.com>
2017-06-19 23:12:24 -0400

    se realizaron correcciones a Mandelbrot

--- Fractal/src/model/Mandelbrot.java
+++ Fractal/src/model/Mandelbrot.java
@@ -1,24 +1,38 @@
 package model;
 
+import java.util.ArrayList;
+
 import javafx.scene.paint.Color;
 
 public class Mandelbrot {
-	private int xs,ys,x,y,iterations;
+	private int iterations;
 	private Color color,background;
 	
 	
-	public Mandelbrot(int xs,int ys){
-		this.xs=xs;
-		this.ys=ys;
+	public Mandelbrot(){		
 		color = Color.DARKGREY;
 		background = Color.WHITE;
 		iterations = 100;
 	}
 	
-	public Color NewValues(int xs, int ys){
+	public ArrayList<Object[]> getPixels(int x0,int xf,int y0,int yf){
+		ArrayList<Object[]> list = new ArrayList<Object[]>();
+		for(int i = x0; i <= xf;i++){
+			for(int j = y0; j <= yf;j++){
+				Object[] e = new Object[3];
+				e[0]=i;
+				e[1]=j;
+				e[2]=getColor(i, j);
+				list.add(e);
+			}
+		}
+		return list;
+	}
+	
+	private Color getColor(int xs, int ys){
 		Color ans = background;
-		x = xs;
-		y = ys;
+		int x = xs;
+		int y = ys;
 		int r = 0;
 		int i = 0;
 		while(r > 10000 || i==iterations){
